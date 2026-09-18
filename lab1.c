ebce13ec185ee20e5c0c4685384c9470fb5f73e3
JennyFUNG-bit <jennyfung0612@gmail.com>
2026-09-17 22:16:26 -0700

    fix the value type

--- lab1.c
+++ lab1.c
@@ -10,7 +10,7 @@ int main() {
   char *word = "";
   if (read != -1) {
 
-    for (int i = 0; i < strlen(sentence); i++) {
+    for (size_t i = 0; i < strlen(sentence); i++) {
       if (sentence[i] == ' ') {
         printf("%s \n", word);
         word = "";
