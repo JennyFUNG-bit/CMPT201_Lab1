cb936d16a101767fc8123772675916aa25355509
JennyFUNG-bit <jennyfung0612@gmail.com>
2026-09-17 22:05:16 -0700

    test for loop

---
+++ lab1.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+  char *sentence = NULL; // Buffer pointer
+  size_t len = 0;        // Buffer size
+  ssize_t read;          // Number of characters read
+  printf("Enter a line: ");
+  read = getline(&sentence, &len, stdin);
+  char *word = "";
+  if (read != -1) {
+
+    for (int i = 0; i < sentence.len; i++) {
+      if (sentence[i] == " ") {
+        printf("%s \n", word);
+        word = "";
+      } else if (i = sentence.len - 1) {
+        printf("%s \n", word);
+        word = "";
+      } else {
+        word += sentence[i];
+        continue;
+      }
+    }
+    printf("You entered: %c", sentence[0]);
+  } else {
+    printf("Error reading input\n");
+  }
+  free(sentence); // Prevent memory leaks
+  return 0;
+}
